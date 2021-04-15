05b27ce70df23c9720795e0b1c552f84f45d629e
Hagen DeRouen <hagend87@gmail.com>
2021-04-15 16:47:28 -0500

    Add read employees table to console functionality.

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-// require('mysql)
+const mysql = require('mysql');
+const cTable = require('console.table');
+require('dotenv').config();
 // requie('inquirer')
-// require('console.table')
 
-// TODO Scaffold database using required structure
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: process.env.MYSQL_PW,
+    database: 'employee_tracker_db',
+});
+
+// CRUD employees
+const createEmployee = () => {
+    console.log('Creating a new employee...\n');
+    // do something
+}
+
+const readEmployees = () => {
+    console.log('Selecting all employees...\n');
+    connection.query(
+        `SELECT e.id, e.first_name, e.last_name, title, salary, d.name AS department, CONCAT_WS(' ', m.first_name, m.last_name) AS manager
+        FROM employee AS e
+            INNER JOIN role as r
+            ON e.role_id = r.id
+            INNER JOIN department as d
+            ON r.department_id = d.id
+            LEFT JOIN employee as m
+            on e.manager_id = m.id;`, (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      connection.end();
+    });
+};
 
 // Departments: sales, engineering, accounting
 // Roles: sales rep, software engineer, accountant
